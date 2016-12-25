4ac1a386f65447c7b58b9af8573dc6f112ca4d9a
Ronald Dahlgren <ronald.dahlgren@gmail.com>
2016-12-25 13:20:22 -0500

    Add logging configuration

--- src/main/groovy/com/stanson/bass/BassDriver.groovy
+++ src/main/groovy/com/stanson/bass/BassDriver.groovy
@@ -1,6 +1,11 @@
 package com.stanson.bass
 
 import groovy.util.logging.Log4j
+import org.apache.log4j.Appender
+import org.apache.log4j.ConsoleAppender
+import org.apache.log4j.Layout
+import org.apache.log4j.Level
+import org.apache.log4j.PatternLayout
 
 /**
  * Created by rdahlgren on 12/25/16.
@@ -15,7 +20,15 @@ class BassDriver {
     }
 
     void configureLogging() {
-        // todo
+        log.level = Level.INFO
+        String pattern = '%d{YYYY-MM-dd HH:mm:ss.SSSS} %c >> %m\n'
+        // NOTE: Using groovy bean-style initialization (pattern: pattern or layout:consoleLayout) will
+        // break these classes. Doing so fails to call the 'activateOptions' internal helper. Poor form
+        // log4j!
+        Layout consoleLayout = new PatternLayout(pattern)
+        Appender console = new ConsoleAppender(consoleLayout)
+        log.getRootLogger().addAppender(console)
+
     }
 
     void driverLogic(BooleanAlgebraSolverService bass) {
