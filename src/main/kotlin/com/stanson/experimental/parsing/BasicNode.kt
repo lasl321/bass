e56547f52dd6c9b3aab22450cf15bf8afb008c47
Luis Sotomayor <luis.sotomayor@stansonhealth.com>
2017-09-02 15:30:35 -0700

    Updated basic node

--- src/main/kotlin/com/stanson/experimental/parsing/BasicNode.kt
+++ src/main/kotlin/com/stanson/experimental/parsing/BasicNode.kt
@@ -1,112 +1,96 @@
 package com.stanson.experimental.parsing
 
-import com.stanson.bass.NodeType
+import com.stanson.experimental.NodeType
 import com.stanson.experimental.TreeLike
-import groovy.transform.CompileStatic
 
-class BasicNode(val parseNodeType: BaseNodeType, val data: Any) : TreeLike<BasicNode> {
-    var parent: BasicNode
+class BasicNode(private val parseNodeType: BaseNodeType, private val data: Any?) : TreeLike<BasicNode> {
+    private var parent: BasicNode? = null
+    private var children = mutableListOf<BasicNode>()
 
-    //var  children:List<BasicNode> = listOf()
+    override fun getChildren(): List<BasicNode> {
+        return children
+    }
 
-    
-
-     fun addChildren(vararg children : BasicNode) :BasicNode{
+    fun addChildren(vararg children: BasicNode): BasicNode {
         children.forEach { this.addChild(it) }
-        this
+        return this
     }
 
-    fun removeAllChildren() :Unit{
-        children.forEach {  this.removeChild(it) }
+    fun removeAllChildren() {
+        getChildren().forEach { this.removeChild(it) }
     }
 
-     fun addChild( child:BasicNode) :BasicNode{
-        if (child.parent) { child.parent.removeChild(child) }
-        children  child
+    override fun addChild(child: BasicNode): BasicNode {
+        if (child.parent != null) {
+            child.parent!!.removeChild(child)
+        }
+        children.add(child)
         child.parent = this
-        this
+        return this
     }
 
-    /**
-     * If the specified child belongs to this node, renounce parenthood and
-     * and kick that child to the curb.
-     */
-    void removeChild(BasicNode child) {
-        children -= child
+    override fun removeChild(child: BasicNode) {
+        children.remove(child)
         child.parent = null
     }
 
-    /**
-     * Adds all provided children to this instance and returns this instance.
-     *
-     * All added children should have their parent link set to this instance.
-     *
-     * @param children
-     * @return this instance
-     * @throws NullPointerException if children == null || children.any { it == null }
-     */
-    @Override
-    BasicNode addChildren(List<BasicNode> children) {
-        children.each { BasicNode child -> this.addChild(child) }
-        this
+    override fun addChildren(children: List<BasicNode>): BasicNode {
+        children.forEach { this.addChild(it) }
+        return this
     }
 
-    /**
-     * True if the data associated with this instance and the other instance are considered
-     * to be equivalent enough to be interchangeable.
-     *
-     * @return true if this.data == other.data
-     */
-    @Override
-    Boolean dataEquivalent(BasicNode other) {
+    override fun dataEquivalent(other: BasicNode): Boolean {
         return this.data == other.data
     }
 
-    /**
-     * Computes and returns the hashcode of the subtree formed by interpreting this node as the root.
-     */
-    int hashCode() {
-        int result = 31
-        result = 17 * result + (nodeType ? nodeType.hashCode() : 0)
-        // Need '!= null' check as '0' is a valid, but falsy data value
-        result = 17 * result + (data != null ? data.hashCode() : 0)
-        result = 17 * result + children.hashCode()
+    override fun hashCode(): Int {
+        var result = 31
+        result = 17 * result + getNodeType().hashCode()
+        result = 17 * result + (data?.hashCode() ?: 0)
+        result = 17 * result + getChildren().hashCode()
         return result
     }
 
-    /**
-     * A == B IFF this node and all children (the subtree) are equal.
-     */
-    boolean equals(Object obj) {
-        if (!(obj instanceof BasicNode)) { return false }
-        BasicNode that = obj as BasicNode
-        return this.nodeType == that.nodeType &&
-                this.data == that.data &&
-                this.children.size() == that.children.size() &&
-                (this.children as Set<BasicNode>) == (that.children as Set<BasicNode>)
+    override fun equals(other: Any?): Boolean {
+        if (other !is BasicNode) {
+            return false
+        }
+
+        return this.getNodeType() == other.getNodeType() &&
+                this.data == other.data &&
+                this.children.size == other.children.size &&
+                (this.children.toSet()) == (other.children.toSet())
     }
 
-    String toString() { toStringHelper('') }
-
-    protected String toStringHelper(String indent) {
-        String dataString = this.data ? "${this.data}" : 'NODATA'
-        String result = "${indent} ${this.nodeType} (${dataString})\n"
-        result += this.children.collect {
-            it.toStringHelper("${indent}>")
-        }.join(",")
-        result
+    override fun toString(): String {
+        return toStringHelper("")
     }
 
-    NodeType getNodeType() {
-        // NULL, ANY, ALL, NOT, PREDICATE, TRUE, FALSE
-        [
-                (BaseNodeType.NULL)     : NodeType.NULL,
-                (BaseNodeType.ANY)      : NodeType.ANY,
-                (BaseNodeType.ALL)      : NodeType.ALL,
-                (BaseNodeType.NOT)      : NodeType.NOT,
-                (BaseNodeType.PREDICATE): NodeType.PREDICATE,
-                (BaseNodeType.TRUE)     : NodeType.TRUE,
-                (BaseNodeType.FALSE)    : NodeType.FALSE,
-        ].get(this.parseNodeType)
+    private fun toStringHelper(indent: String): String {
+        val dataString = if (this.data != null) {
+            "${this.data}"
+        } else {
+            "NODATA"
+        }
+
+        var result = "$indent ${this.getNodeType()} ($dataString)\n"
+        result += this.children.joinToString(",") {
+            it.toStringHelper("$indent>")
+        }
+
+        return result
+    }
+
+    override fun getNodeType(): NodeType {
+        val m = mapOf(
+                BaseNodeType.NULL to NodeType.NULL,
+                BaseNodeType.ANY to NodeType.ANY,
+                BaseNodeType.ALL to NodeType.ALL,
+                BaseNodeType.NOT to NodeType.NOT,
+                BaseNodeType.PREDICATE to NodeType.PREDICATE,
+                BaseNodeType.TRUE to NodeType.TRUE,
+                BaseNodeType.FALSE to NodeType.FALSE)
+
+        return m[parseNodeType]!!
     }
 }
