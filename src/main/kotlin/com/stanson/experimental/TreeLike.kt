594b2fdc1f01da03befc402fd8a4f780b9bbf42c
Luis Sotomayor <luis.sotomayor@stansonhealth.com>
2017-09-03 13:32:53 -0700

    :x

--- src/main/kotlin/com/stanson/experimental/TreeLike.kt
+++ src/main/kotlin/com/stanson/experimental/TreeLike.kt
@@ -3,9 +3,9 @@ package com.stanson.experimental
 interface TreeLike<T> where T : TreeLike<T> {
     val nodeType: NodeType
     val children: List<T>
-    fun removeChild(child: T)
-    fun addChildren(children: List<T>): T
     fun addChild(child: T): T
+    fun addChildren(children: List<T>): T
+    fun removeChild(child: T)
     fun dataEquivalent(other: T): Boolean
 }
 
